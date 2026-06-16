f8acbcbda6a89311c30bf9a31e506eb3a8daab62
Jonathan-Pruitt <jonathan.s.pruitt@gmail.com>
2026-06-16 13:08:27 -0500

    feat: ADDED COMPONENTS AND A SECTION FOR GRANULAR LAYOUT CONTROL

---
+++ src/sections/Experience.vue
@@ -0,0 +1,74 @@
+<script setup>
+const sectionId = '#'
+</script>
+
+<template>
+  <section :id="sectionId">
+    <!-- EXPERIENCE -->
+    <div class="border-2">
+      <h3>Tech Experinece</h3>
+      <h4 class="">
+        Core Competencies 
+        <button 
+          @click="handleClickCategories('core')"
+          class="text-xs cursor-pointer text-info"
+        >
+          {{ showDetails != 'core' ? ' more' : ' less' }}
+        </button>
+        <em 
+          class="italic text-xs text-body transition-opacity duration-75"
+          :class="showDetails == 'core' ? 'visible opacity-100' : 'hidden opacity-0'"
+        >
+          Technologies I use independently to design, build, and deliver production-ready projects with minimal oversight (other than code reivew).
+        </em>
+      </h4>
+      <div class="grid grid-cols-2">
+        <div class=""
+          v-for="tag in coreTags"
+        >
+          <div class="grid grid-cols-2 items-center">
+            <div class="size-8 sm:size-12 md:size-16 justify-self-end box-content p-1">
+              <TechTag 
+                :tag="tag"
+              />
+            </div>
+            <div class="dark:border-none border-subtle">
+              <h5 class="font-semibold tracking-tight">{{ tag.title }}</h5>
+              <p  class="text-sm text-content font-light">{{ tag.useFrequency }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <h4 class="">
+        Proficient
+        <button 
+          @click="handleClickCategories('proficient')"
+          class="text-xs cursor-pointer text-info"
+        >
+          {{showDetails != 'proficient' ? 'more' : 'less'}}
+        </button>
+        <em 
+          class="italic text-xs text-body"
+          :class="showDetails == 'proficient' ? 'visible opacity-100' : 'hidden opacity-0'"
+        >
+          Technologies applied successfully in personal or hobby projects where I can build functional applications independently.
+        </em>
+      </h4>
+      <h4 class="">
+        Familiar 
+        <button 
+          @click="handleClickCategories('familiar')"
+          class="text-xs cursor-pointer text-info"
+        >
+          {{showDetails != 'familiar' ? 'more' : 'less'}}
+        </button>
+      </h4>
+      <em 
+        class="italic text-xs text-body tracking-tight"
+        :class="showDetails == 'familiar' ? 'visible opacity-100' : 'hidden opacity-0'"
+      >
+        Technologies I have conceptual knowledge of and basic hands-on experience with through limited project exposure.
+      </em>
+    </div>
+  </section>
+</template>
---
+++ src/sections/components/ProjectItem.vue
@@ -0,0 +1,199 @@
+<script setup>
+import { computed, onMounted, ref } from 'vue';
+import Carousel from './Carousel.vue';
+import TechTag from '../partials/TechTag.vue';
+import Tag from '../partials/Tag.vue';
+import tagSvg from '/src/assets/icons/tags/tag.svg?component';
+import techTagSvg from '/src/assets/icons/tags/tech.svg?component';
+import { TechTagObject } from '/src/services/TechTagObject.js';
+
+const props = defineProps({
+  project: Object,
+  maxTechItems: Number
+})
+const emit = defineEmits(['update-max-tech'])
+const project = ref(props.project);
+const techTags = ref([]);
+// const maxTagCount = ref(0); 
+const COLORS = {
+    "backend" :             'bg-[hsl(0,50%,50%)]/20 inset-ring-[hsl(0,50%,50%)] text-[hsl(0,50%,50%)]',
+    "frontend" :            'bg-[hsl(216,50%,50%)]/20 inset-ring-[hsl(216,50%,50%)] text-[hsl(216,50%,50%)]',
+    "framework" :           'bg-[hsl(144,70%,40%)]/20 inset-ring-[hsl(144,70%,40%)] text-[hsl(144,70%,40%)]',
+    "version control" :     'bg-[hsl(288,50%,50%)]/20 inset-ring-[hsl(288,50%,50%)] text-[hsl(288,50%,50%)]',
+    "project management":   'bg-[hsl(72,50%,40%)]/20 inset-ring-[hsl(72,50%,40%)] text-[hsl(72,50%,40%)]',
+}
+
+const maxLogos = computed(() => {
+    if (!props.maxTechItems) {return 'grid-cols-1'}
+    if (props.maxTechItems > 3) {return 'grid-cols-3'}
+    return 'grid-cols-' + props.maxTechItems;
+})
+
+// METHODS
+
+const loadTechTags = () => {
+    const tags = project.value.techStack.map((tag) => new TechTagObject(tag).getTechTagItem())
+    let maxTags = 0
+    techTags.value.domains = {};
+    const fEnd = tags.filter((t) => t.domain == 'frontend');
+    const bEnd = tags.filter((t) => t.domain == 'backend');
+    const frame = tags.filter((t) => t.domain == 'framework');
+    const pMan = tags.filter((t) => t.domain == 'project management');
+    const vCont = tags.filter((t) => t.domain == 'version control');
+
+    addTags(fEnd);
+    addTags(bEnd);
+    addTags(frame);
+    addTags(pMan);
+    addTags(vCont);
+    emit('update-max-tech', maxTags)
+    console.log(techTags.value)
+
+    function addTags(tagArray) {
+        if (tagArray.length > 0) {
+            const key = tagArray[0].domain
+            // maxTagCount.value = Math.max(tagArray.length, maxTagCount.value)
+            maxTags = Math.max(tagArray.length, maxTags)
+            techTags.value[key] = tagArray;
+        }
+    }
+}
+
+const capitalizeFirstChar = (text) => {
+    return text.charAt(0).toUpperCase() + text.slice(1);
+}
+
+onMounted(() => {
+    loadTechTags();
+    // emit('update-max-tech', maxTagCount.value)
+})
+
+</script>
+
+<template>
+    <!-- PROJECT CONTAINER -->
+    <div class="w-xs mx-auto bg-linear-to-t from-flat to-raised rounded-lg shadow-lg shadow-brand/20">
+        <div class="w-2xs mx-auto grid grid-cols-3 gap-2 overflow">
+            
+            <!-- ROW 1 -->
+            <div class="col-span-full">
+                <a 
+                    :href="project.links.sample" 
+                    target="_blank" 
+                    class="flex gap-2 items-center"
+                >
+                    <img :src="project.iconPath" class="size-8 sm:size-16" :alt="project.title + ' logo'">
+                    <h3 class="text-2xl font-bold">{{ project.title }}</h3>
+                </a>
+            </div>
+            
+            <!-- ROW 2 -->
+            <div class="col-span-1 content-center">
+
+                <div class="flex text-xs justify-center border-b border-brand/30">
+                    <techTagSvg class="size-4 fill-gray-500/75" />
+                    <h4 class="text-gray-500/75">Tech</h4>
+                </div>
+                <div v-if="techTags.frontend">
+                    <h5 class="text-sm font-semibold">Frontend</h5>
+                    <div class="grid" :class="maxLogos">
+                        <TechTag 
+                            v-for="frontendTag in techTags.frontend"
+                            :tag="frontendTag"
+                        />
+                    </div>
+                </div>
+                <div v-if="techTags.backend">
+                    <h5 class="text-sm font-semibold">Backend</h5>
+                    <div class="grid" :class="maxLogos">
+                        <TechTag 
+                            v-for="backendTag in techTags.backend"
+                            :tag="backendTag"
+                        />
+                    </div>
+                </div>
+                <div v-if="techTags.framework">
+                    <h5 class="text-sm font-semibold">Framework</h5>
+                    <div class="grid" :class="maxLogos">
+                        <TechTag 
+                            v-for="frameworkTag in techTags.framework"
+                            :tag="frameworkTag"
+                        />
+                    </div>
+                </div>
+                <div v-if="techTags['version control']">
+                    <h5 class="text-sm font-semibold">Versioning</h5>
+                    <div class="grid" :class="maxLogos">
+                        <TechTag 
+                            v-for="versionTag in techTags['version control']"
+                            :tag="versionTag"
+                        />
+                    </div>
+                </div>
+                <div v-if="techTags['project management']">
+                    <h5 class="text-sm font-semibold">Project Mgmt</h5>
+                    <div class="grid" :class="maxLogos">
+                        <TechTag 
+                            v-for="projectTag in techTags['project management']"
+                            :tag="projectTag"
+                        />
+                    </div>
+                </div>
+                <!-- <ul class="flex flex-col">
+                    <li
+                        v-for="tag in techTags"
+                    >
+                        <div 
+                            class="size-8 sm:size-12 lg:size-16"
+                        >
+                            <TechTag :tag="tag"/>
+                        </div>
+                        <h5 
+                            class="text-xs font-semibold px-2 py-1 rounded-lg inset-ring-2 text-center hidden md:block mx-auto"
+                            :class="COLORS[tag.domain]"
+                        >
+                            {{ tag.domain }}
+                        </h5>
+                    </li>
+                </ul> -->
+            </div>
+            <div class="col-span-2">
+                <Carousel 
+                    :slides="project.images"
+                />
+            </div>
+        
+            <!-- ROW 3 -->
+            <div class="col-span-2">
+                <p class="font-light text-sm">{{ project.description }}</p>
+            </div>
+            <div class="col-span-1 content-center">
+                <div class="flex text-xs justify-center border-b border-brand/30">
+                    <tagSvg class="size-4 fill-gray-500/75" />
+                    <h4 class="text-gray-500/75">Tags</h4>
+                </div>
+                <ul class="flex flex-col">
+                    <li 
+                        class="flex flex-wrap justify-center"
+                    >
+                    <Tag 
+                        v-for="tag in project.tags"
+                        :tag="tag" 
+                    />
+                    </li>
+                </ul>
+            </div>
+            <div 
+                v-if="project.disclaimer"
+                class="col-span-full"
+            >
+                <p 
+                    class="text-xs text-gray-500/60 italic my-2"
+                    :class="project.disclaimer ? 'mb-4' : ''"
+                >
+                    {{ project.disclaimer ?? '' }}
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
