ee8b54922c0c1d1337feb8f2be23f146916f11c9
Jonathan-Pruitt <jonathan.s.pruitt@gmail.com>
2026-06-21 21:16:10 -0500

    feat: Add proficiencies component and styles for less redundancy

---
+++ src/sections/Proficiencies.vue
@@ -0,0 +1,104 @@
+<script setup>
+import Proficiency from './components/Proficiency.vue';
+import SvgIcon from './partials/SvgIcon.vue';
+import TechTag from './partials/TechTag.vue';
+import { TechTagObject } from '../services/TechTagObject.js';
+import { onMounted, ref } from 'vue';
+
+// STATIC DATA
+const coreArray = [
+  'github',
+  'css',
+  'html',
+  'javascript',
+  'laravel',
+  'linear',
+  'linux',
+  'windows',
+  'tailwind',
+  'php',
+  'vue',
+];
+const proficientArray = [
+  'csharp',
+  'python',
+  'mysql',
+  'xaml',
+  'wpf'
+];
+const familiarArray = [
+  'react',
+];
+
+// DATA
+const props = defineProps({
+  sectionId: String
+})
+
+const showDetails = ref('');
+const coreTags = ref([]);
+const proficientTags = ref([]);
+const familiarTags = ref([]);
+
+const prof = {
+  title: "Proficient",
+  description: "Can confidently build, debug, and deploy full features independently using pre-defined best practices."
+}
+const comf = {
+  title: "Comfortable",
+  description: "Possesses a solid foundational understanding and can regularly contribute to existing codebases"
+}
+const fami = {
+  title: "Familiar",
+  description: "Possess conceptual knowledge of these technologies and basic hands-on experience through limited project exposure"
+}
+
+// METHODS
+const getAllTags = () => {
+  coreTags.value = getTagArray(coreArray);
+  proficientTags.value = getTagArray(proficientArray);
+  familiarTags.value = getTagArray(familiarArray);
+}
+
+const getTagArray = (stringArray) => {
+  let tagArray = stringArray.map((rawTag) => new TechTagObject(rawTag).getTechTagItem())
+  return tagArray;
+}
+
+const handleClickCategories = (category) => {
+  if (category == showDetails.value) {
+    showDetails.value = ''
+  } else {
+    showDetails.value = category
+  }
+}
+
+onMounted(() => {
+  getAllTags();
+})
+</script>
+
+<template>
+  <section :id="sectionId">
+    <!-- Proficiencies -->
+    <h2 class="text-4xl font-black cursor-default">PROFICIENCIES</h2>
+    <div class="m-8">
+      <div 
+        v-if="coreTags.length > 0"
+      >
+        <Proficiency :category="prof.title" :description="prof.description" :tags="coreTags"/>
+      </div>
+
+      <div 
+        v-if="proficientTags.length > 0"
+      >
+        <Proficiency :category="comf.title" :description="comf.description" :tags="proficientTags"/>
+      </div>
+      <div 
+        v-if="familiarTags.length > 0"
+      >
+        <Proficiency :category="fami.title" :description="fami.description" :tags="familiarTags" />
+      </div>
+    </div>
+  </section>
+</template>
---
+++ src/sections/components/Proficiency.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { ref } from 'vue';
+import TechTag from '../partials/TechTag.vue';
+
+const props = defineProps({
+  category: String,
+  description: String,
+  tags: Array
+})
+
+const showDetails = ref(false);
+</script>
+
+<template>
+  <div class="rounded-lg shadow-lg my-8 px-8 py-4 dark:bg-base/10 dark:inset-shadow-sm dark:inset-shadow-gray-500/50">
+    <h3 class="text-xl font-bold tracking-wider">
+      {{ category }}
+      <button 
+        @click="showDetails = !showDetails"
+        class="text-xs cursor-pointer text-info"
+      >
+        {{ showDetails != true ? ' more' : ' less' }}
+      </button>
+    </h3>
+    <em 
+      class="italic text-xs text-body transition-opacity duration-75 tracking-wider"
+      :class="showDetails == true ? 'visible opacity-100' : 'hidden opacity-0'"
+      >
+      {{ description }}
+    </em>
+    <div class="grid grid-cols-2">
+      <div class="justify-self-start"
+        v-for="tag in tags"
+      >
+        <div class="flex items-center">
+          <div class="size-8 sm:size-12 md:size-16 box-content p-1 dark:bg-linear-to-br dark:to-info/40 dark:from-brand/20 rounded sm:rounded-lg">
+            <TechTag 
+              :tag="tag"
+            />
+          </div>
+          <div class="dark:border-none border-subtle">
+            <h5 class="font-semibold tracking-tight">{{ tag.title }}</h5>
+            <p  class="text-sm text-content font-light">{{ tag.useFrequency }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
